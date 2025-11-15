b8672953e496b3d8f5ed8c57052922735b419edb
Hrishav Raj <141990450+hriiishav@users.noreply.github.com>
2025-11-15 17:31:21 +0530

    Initial Commit

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cookieParser from "cookie-parser";
+
+const app=express();
+app.use(express.json());
+app.use(cookieParser());
+
+const PORT = process.env.PORT;
+
+app.listen(PORT,()=>{
+  console.log("Server is running on port : "+PORT);
+})
