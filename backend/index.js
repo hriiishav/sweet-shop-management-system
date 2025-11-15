e43a898c6743de7783922fa952c7656106d5b593
Hrishav Raj <141990450+hriiishav@users.noreply.github.com>
2025-11-15 19:30:40 +0530

    Installed bcryptjs, ioredis and jsonwebtoken packages (had forgotten before). Set up redis(caching) to store refresh-tokens (will add a few other frequently used data in redis afterwards). Included the authorization routes and logic using route-controller setup.

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,15 @@ import express from "express";
 import cookieParser from "cookie-parser";
 
 import { connectDB } from "./utils/db.js";
+import authRoutes from "./routes/auth.route.js";
 
 const app=express();
 app.use(express.json());
 app.use(cookieParser());
 
 const PORT = process.env.PORT;
+
+app.use("/api/auth",authRoutes);
 
 app.listen(PORT,()=>{
   console.log("Server is running on port : "+PORT);
